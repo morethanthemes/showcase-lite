9f76394008ac3c2ef7aab176ba35383ca5b21928
Bill Kechagias <vkechagias@gmail.com>
2017-03-13 16:40:42 +0200

    chore(js): remove unused js files

--- site/themes/contrib/showcase_lite/js/custom/video-bg.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function($) {
-  var color = $(".media-background").css("background-color").replace(")", "," + drupalSettings.showcaseplus.VideoBg.VideoBgOpacity + ")").replace("rgb", "rgba");
-  $(".media-background-transparent-bg").css("background-color", color);
-});
--- site/themes/contrib/showcase_lite/js/init/owl-carousel-brands-init.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-  $(".mt-carousel-brands").owlCarousel({
-    itemsCustom: [[0, 2], [480, 3], [768, 4], [992, 5], [1200, 5]],
-    autoPlay: drupalSettings.showcaseplus.owlCarouselBrandsInit.owlBrandsEffectTime,
-    navigation: true,
-    pagination: false,
-    navigationText: false
-  });
-});
